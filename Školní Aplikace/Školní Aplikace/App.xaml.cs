fec130607d0b417f3a0576d5b7ef9ff939486130
OldaLP <78831891+OldaLP@users.noreply.github.com>
2022-05-08 23:39:27 +0200

    Přidejte soubory projektu.

---
+++ Školní Aplikace/Školní Aplikace/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Školní_Aplikace
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+
+            List<Uzel> List_Pointeru_Bodu = new List<Uzel>();
+            Uzel Graph_Bod_1 = new Uzel();
+            List_Pointeru_Bodu.Add(Graph_Bod_1);
+            System.Console.Write("AAAA");
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Školní Aplikace/Školní Aplikace/Graph.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Školní_Aplikace
+{
+
+    public class Uzel
+    {
+        List<Connector> napojeni;
+        public Uzel()
+        {
+            napojeni = new List<Connector>();
+        }
+        public void Connection(Pointer Connected_Uzel, float distance)
+        {
+            Connector conection = new Connector(Connected_Uzel,distance);
+            napojeni.Add(conection);
+
+        }
+    }
+
+    public class Connector
+    {
+        public Connector(Pointer Connection, float distance)
+        {
+            Pointer pointer = Connection;
+            float distanceX = distance;
+
+
+        }
+    }
+
+
+
+}
